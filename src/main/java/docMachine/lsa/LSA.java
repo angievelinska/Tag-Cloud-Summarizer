bf94e1c1e96b2806ac477baae26745a7508a6179
angelina velinska <angelina.velinska@coremedia.com>
2010-08-26 09:22:37 +0200

    log system properties. creating matrix in lsa doesnt work yet.

--- src/main/java/docMachine/lsa/LSA.java
+++ src/main/java/docMachine/lsa/LSA.java
@@ -6,43 +6,27 @@ import edu.ucla.sspace.lsa.LatentSemanticAnalysis;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.Properties;
 
 /**
  * User: avelinsk
  * Date: 11.08.2010
  *
- * Class creates a Semantic Space using Latent Semantic Analysis (LSA) algo.
+ * The class creates a Semantic Space using Latent Semantic Analysis (LSA) algorithm.
  */
 public class LSA {
-//    protected ArgOptions args;
     private final static Log log = LogFactory.getLog(LSA.class);
 
-    public LatentSemanticAnalysis invokeLSA() throws IOException{
-/*        String[] options = {"-d",
-                    "C:\\master_thesis\\TagCloudSummarizer\\output\\input.txt",
-                    "--tokenFilter",
-                    "exclude=stopwords\\english-stop-words-large.txt",
-                    "-w",
-                    "true",
-                    "-Z",
-                    "edu.ucla.sspace.txt.EnglishStemmer",
-                    "-o",
-                    "text",
-                    "-S",
-                    "SVDLIBJ",
-                    "C:\\master_thesis\\TagCloudSummarizer\\output\\LSA_text.sspace"};*/
-
-
+    protected LatentSemanticAnalysis invokeLSA() throws IOException{
         //add all documents to the semantic space and call processSpace
         Properties props = new Properties();
         props.putAll(initProperties());
         LatentSemanticAnalysis lsa = new LatentSemanticAnalysis(props);
-//        lsa.processSpace(props);
-
+//        this.parseProps();
         return lsa;
     }
 
@@ -74,15 +58,16 @@ public class LSA {
         props.put("overwrite","true");
         props.put("tokenFilter","exclude=C:\\master_thesis\\TagCloudSummarizer\\stopwords\\english-stop-words-large.txt");
         props.put("verbose","true");
-        props.put("","");
 
         return props;
     }
 
-    protected File initOutputFile(){
+    protected File initOutputFile() throws IOException{
         File outputPath = new File("C:\\master_thesis\\TagCloudSummarizer\\sspace");
         File outputFile = new File(outputPath,"LSA.sspace");
-
+        if (!outputFile.exists()){
+            outputFile.createNewFile();
+        }
         return outputFile;
     }
 
@@ -90,9 +75,37 @@ public class LSA {
         long start = System.currentTimeMillis();
         SemanticSpace sspace = this.invokeLSA();
         File output = this.initOutputFile();
-        SemanticSpaceIO.save(sspace, output);
+        DataInputStream dis= new DataInputStream(new BufferedInputStream(new FileInputStream(output)));
+        SemanticSpaceIO.save(sspace, output, SemanticSpaceIO.SSpaceFormat.TEXT);
         long end = System.currentTimeMillis();
+        log.info("-------------INFO---------------");
         log.info("LSA used "+(end-start)+"ms to index the document collection.");
+        log.info("Name of semantic algorithm:"+sspace.getSpaceName());
+        log.info("Number of words in the sspace: "+sspace.getWords().size());
+        this.parseProps();
+        log.info("-------------END----------------");
+    }
+
+    protected void parseDocuments(){
+        Properties props = System.getProperties();
+        Iterator it = props.stringPropertyNames().iterator();
+/*        for (Map.Entry st : props){
+
+        }
+                .next().equalsIgnoreCase()
+        String docFile =  props.getProperty("docFile");
+        log.info("docFile: "+docFie);
+        Iterator<Document> docIter =*/ 
+    }
+
+    protected void parseProps(){
+        Properties sysprops = System.getProperties();
+        log.info("Properties:");
+        for(Enumeration en = sysprops.propertyNames(); en.hasMoreElements(); ){
+            String key = (String) en.nextElement();
+            String value = sysprops.getProperty(key);
+            log.info(key+" = "+value);
+        }
     }
 
 }
