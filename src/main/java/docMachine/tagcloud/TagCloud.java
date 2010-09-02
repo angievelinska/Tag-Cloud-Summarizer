866e9bd970938f01cf21dc87830609635cad9a0f
angelina velinska <an.velinska@gmail.com>
2010-09-03 00:16:45 +0200

    try to generate a tag cloud . not working.

--- src/main/java/docMachine/tagcloud/TagCloud.java
+++ src/main/java/docMachine/tagcloud/TagCloud.java
@@ -8,13 +8,26 @@ import org.mcavallo.opencloud.Tag;
  * Date: 01.09.2010
  */
 public class TagCloud {
-  public void createCloud(){
+  public Cloud createCloud(){
     Cloud cloud = new Cloud();
     cloud.setMaxWeight(38.0);
+    return cloud;
   }
 
   public void populateCloud(Cloud cloud){
-    Tag tag = new Tag("CoreMedia", "https://www.coremedia.com/");
+    Tag tag = new Tag("CoreMedia", "https://www.coremedia.com/", 3.5);
     cloud.addTag(tag);
+  }
+
+  public void orderCloud(Cloud cloud){
+    cloud.tags(new Tag.ScoreComparatorDesc());
+    cloud.setThreshold(2.0);
+  }
+
+  public static void main(String[] args){
+    TagCloud tc = new TagCloud();
+    Cloud c = tc.createCloud();
+    tc.populateCloud(c);
+    tc.orderCloud(c);
   }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,10 +1,11 @@
-<%@ page import="javax.swing.text.html.HTML" %>
+<%@ page import="org.mcavallo.opencloud.Cloud" %>
+<%@ page import="org.mcavallo.opencloud.Tag" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head><title>Simple jsp page</title></head>
   <body>
     <div>
-    <% for (HTML.Tag tag : cloud.tags()) { %>
+    <% for (Tag tag : c.tags()) { %>
     <a href="<%= tag.getLink() %>" style="font-size: <%= tag.getWeight() %>px;"%gt;<%= tag.getName() %></a>
     <% } %>
   </div>
