6152770395b6568573d3e41a7dfbb1516107283b
angelina velinska <an.velinska@gmail.com>
2010-10-08 10:21:38 +0200

    project changes

---
+++ src/main/java/docMachine/lsa/Query.java
@@ -0,0 +1,25 @@
+package docMachine.lsa;
+
+import edu.ucla.sspace.common.DocumentVectorBuilder;
+
+/**
+ * User: avelinsk
+ * Date: 02.10.2010
+ * Time: 17:27:26
+ */
+public class Query {
+
+  private String query;
+
+  private DocumentVectorBuilder docBuilder;
+
+  protected void setQuery(String query){
+    query = query;
+  }
+
+
+
+  
+
+
+}
