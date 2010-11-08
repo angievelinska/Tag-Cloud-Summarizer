2206d916e2f8f6e4e7e09a11592de8dfcc4c6378
Angie <myemail@home>
2010-11-08 01:55:38 +0100

    fixed deletion of tests by mistake

---
+++ src/test/java/edu/tuhh/tagcloudsummarizer/search/IndexerTest.java
@@ -0,0 +1,18 @@
+package edu.tuhh.tagcloudsummarizer.search;
+
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ng
+ * Date: 17.08.2010
+ * Time: 22:15:52
+ */
+public class IndexerTest {
+
+    @Test
+    public void testIndexDocuments() throws Exception {
+        String[]dirs = {"index","output"};
+        Indexer.indexDocuments(dirs);
+    }
+}
